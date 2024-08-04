77bd52fcfa6f87011b759d036847c9906cf7646b
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-08-04 20:24:20 +0400

    make the inputs controlled

--- client/src/authentication/components/login_form.tsx
+++ client/src/authentication/components/login_form.tsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const LoginForm = () => {
+  const [email_phone, setEmail_Phone] = useState<string | number>('')
+  const [password, setPassword] = useState<string>('')
+
   return (
     <div className='bg-[#313338] rounded-md px-7 py-6 z-[50] absolute w-[500px] top-1/3 flex left-1/2 translate-x-[-50%] flex-col items-center gap-4'>
       <div className='flex flex-col items-center gap-2'>
@@ -17,6 +21,10 @@ const LoginForm = () => {
             <span className='text-[8px] text-red-500'>*</span>
           </label>
           <input
+            value={email_phone}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail_Phone(e.target.value)
+            }
             type='text'
             id='email_phone'
             className='outline-none bg-[#1E1F22] px-3 py-1.5 rounded-[4px] w-full text-white'
@@ -31,6 +39,10 @@ const LoginForm = () => {
               PASSWORD <span className='text-[8px] text-red-500'>*</span>
             </label>
             <input
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
               type='password'
               id='password'
               className='outline-none bg-[#1E1F22] px-3 py-1.5 rounded-[4px] w-full text-white'
