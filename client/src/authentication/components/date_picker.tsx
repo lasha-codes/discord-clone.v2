960d58e971a5a13f88ddd32bf496a589d515e0ec
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-08-05 05:02:52 +0400

    date picker full functionality is done

--- client/src/authentication/components/date_picker.tsx
+++ client/src/authentication/components/date_picker.tsx
@@ -1,23 +1,54 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { IoIosArrowDown } from 'react-icons/io'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  select_date_picker,
+  close_date_picker,
+} from '../../library/slices/navigation'
 
 const DatePicker = ({
   date,
   setDate,
   data,
+  actual_date,
 }: {
   date: string
   setDate: React.SetStateAction<any>
   data: string[] | number[]
+  actual_date: any
 }) => {
+  const dispatch = useDispatch()
+  const { date_picker }: { date_picker: string | null } = useSelector(
+    (state: any) => state.navigation
+  )
+
+  const whenIsOpen = () => {
+    if (date.toLowerCase() === date_picker?.toLowerCase()) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const open: boolean = whenIsOpen()
+
   return (
     <div className='bg-[#1E1F22] px-3 rounded-[5px] py-2 flex w-[125px] items-center justify-between relative'>
-      <div className='absolute w-full bg-[#2b2b30] date_picker border rounded-b-[5px] border-black top-[-200px] left-0 h-[200px] overflow-y-scroll'>
+      <div
+        className={`absolute w-full bg-[#2b2b30] date_picker border rounded-b-[5px] border-black top-[-200px] left-0 h-[200px] overflow-y-scroll transition-all duration-[70ms] shadow-sm shadow-black ${
+          open
+            ? 'opacity-100 pointer-events-auto'
+            : 'opacity-0 pointer-events-none'
+        }`}
+      >
         {data.map((date, idx: number) => {
           return (
             <div
               key={idx}
-              onClick={() => setDate(date)}
+              onClick={() => {
+                setDate(date)
+                dispatch(close_date_picker())
+              }}
               className='text-gray-300 py-2 hover:bg-gray-500/15 transition-all duration-[30ms] px-4 cursor-pointer hover:text-white'
             >
               {date}
@@ -25,8 +56,13 @@ const DatePicker = ({
           )
         })}
       </div>
-      <span className='text-[#8E959E]'>{date}</span>
-      <IoIosArrowDown className='text-white cursor-pointer' />
+      <span className='text-[#8E959E]'>{actual_date || date}</span>
+      <IoIosArrowDown
+        onClick={() =>
+          dispatch(select_date_picker({ date: date.toLowerCase() }))
+        }
+        className='text-gray-300 hover:text-white transition-all duration-[100ms] cursor-pointer'
+      />
     </div>
   )
 }
--- client/src/library/slices/navigation.ts
+++ client/src/library/slices/navigation.ts
@@ -3,11 +3,13 @@ import { createSlice } from '@reduxjs/toolkit'
 type initial_type = {
   selected_server: number | null
   direct_messages_open: boolean
+  date_picker: 'month' | 'day' | 'year' | null
 }
 
 const initial_state: initial_type = {
   selected_server: null,
   direct_messages_open: false,
+  date_picker: null,
 }
 
 const navigationSlice = createSlice({
@@ -20,9 +22,21 @@ const navigationSlice = createSlice({
     ) => {
       state.direct_messages_open = bool
     },
+    select_date_picker: (state, { payload }) => {
+      const { date }: { date: 'month' | 'day' | 'year' } = payload
+      if (state.date_picker === date) {
+        state.date_picker = null
+      } else {
+        state.date_picker = date
+      }
+    },
+    close_date_picker: (state) => {
+      state.date_picker = null
+    },
   },
 })
 
 export default navigationSlice.reducer
 
-export const { toggle_direct_messages } = navigationSlice.actions
+export const { toggle_direct_messages, select_date_picker, close_date_picker } =
+  navigationSlice.actions
